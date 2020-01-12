068472f50e67a52c2056f4606cecbb32693ed421
Administrator <792342548@qq.com>
2020-01-12 22:38:08 +0800

    添加beego+eiam+过滤器

---
+++ golangbeego/src/esee-api/routers/commentsRouter_controllers.go
@@ -0,0 +1,46 @@
+package routers
+
+import (
+	"github.com/astaxie/beego"
+	"github.com/astaxie/beego/context/param"
+)
+
+func init() {
+
+	beego.GlobalControllerRouter["esee-api/controllers:ObjectController"] = append(beego.GlobalControllerRouter["esee-api/controllers:ObjectController"],
+		beego.ControllerComments{
+			Method:           "GetAll",
+			Router:           `/eiam`,
+			AllowHTTPMethods: []string{"get"},
+			MethodParams:     param.Make(),
+			Filters:          nil,
+			Params:           nil})
+
+	beego.GlobalControllerRouter["esee-api/controllers:UserController"] = append(beego.GlobalControllerRouter["esee-api/controllers:UserController"],
+		beego.ControllerComments{
+			Method:           "Post",
+			Router:           `/`,
+			AllowHTTPMethods: []string{"post"},
+			MethodParams:     param.Make(),
+			Filters:          nil,
+			Params:           nil})
+
+	beego.GlobalControllerRouter["esee-api/controllers:UserController"] = append(beego.GlobalControllerRouter["esee-api/controllers:UserController"],
+		beego.ControllerComments{
+			Method:           "GetAll",
+			Router:           `/`,
+			AllowHTTPMethods: []string{"get"},
+			MethodParams:     param.Make(),
+			Filters:          nil,
+			Params:           nil})
+
+	beego.GlobalControllerRouter["esee-api/controllers:UserController"] = append(beego.GlobalControllerRouter["esee-api/controllers:UserController"],
+		beego.ControllerComments{
+			Method:           "Logout",
+			Router:           `/logout`,
+			AllowHTTPMethods: []string{"get"},
+			MethodParams:     param.Make(),
+			Filters:          nil,
+			Params:           nil})
+
+}
